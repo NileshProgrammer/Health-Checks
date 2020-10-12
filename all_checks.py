563d038d49d399512b4cd25fe7a00fe6b49a68df
NileshProgrammer <nilesh24rawool@gmail.com>
2020-10-12 20:33:38 +0530

    Added 1 function

--- all_checks.py
+++ all_checks.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python3
 import os
 import sys
+import shutil
 
 # Check Reboot function
 def check_reboot():
     """ Return True if the computer has pending reboot."""
     return os.path.exists("/run/reboot-requires")
+
+
+def check_disk_usage(disk):
+    """Verifies that there's enough free space on disk"""
+    du = shutil.disk_usage(disk)
+    free = du.free / du.total * 100
+    return free > 20
 
 
 # Main Function
